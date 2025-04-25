d5e79cdf894574558c258b48a46b0b45df3a6918
EliNygard <eli.nygard@gmail.com>
2025-04-25 15:28:44 +0200

    removes redundant html elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,17 +8,11 @@ export default async function Home() {
 
   return (
     <main className="flex flex-col gap-[32px]">
-      
-      <div>
-
-      <ul>
-        <ul className="grid gap-6 grid-cols-1 min-[440px]:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
-          {data.map((venue: IVenue) => (
-            <VenueCard key={venue.id} venue={venue} />
-          ))}
-        </ul>
-      </ul>
-          </div>
+          <ul className="grid gap-6 grid-cols-1 min-[440px]:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+            {data.map((venue: IVenue) => (
+              <VenueCard key={venue.id} venue={venue} />
+            ))}
+          </ul>
     </main>
   );
 }
