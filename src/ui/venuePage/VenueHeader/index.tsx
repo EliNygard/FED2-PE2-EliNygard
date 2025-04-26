fae4db9894cee4f13b4a4ca4d40070f8f8075727
EliNygard <eli.nygard@gmail.com>
2025-04-26 15:05:39 +0200

    adds styling

--- src/ui/venuePage/VenueHeader/index.tsx
+++ src/ui/venuePage/VenueHeader/index.tsx
@@ -1,7 +1,32 @@
 import { IVenue } from "@/interface";
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./styles.module.css";
 
-export default function VenueHeader({venue} :{venue: IVenue}) {
+export default function VenueHeader({ venue }: { venue: IVenue }) {
   return (
-    <h1>${venue.name}</h1>
-  )
+    <header className={styles.header}>
+      <section>
+        <h1>{venue.name}</h1>
+        <h2>{`${venue.location.city}, ${venue.location.country}`}</h2>
+      </section>
+      <div className={styles.headerDetails}>
+        <div>
+          <Link href="/profile" className={styles.hostWrapper}>
+            <div className="relative rounded-full h-10 w-10 overflow-hidden">
+              <Image
+                src={venue.owner.avatar.url || "/default-user.svg"}
+                alt={venue.owner.name || "User profile image"}
+                fill
+                sizes="33vw"
+                className="w-full object-cover rounded-full"
+              />
+            </div>
+            <p>{`${venue.owner.name} is hosting`}</p>
+          </Link>
+        </div>
+        <p>{`${venue.price} NOK per night`}</p>
+      </div>
+    </header>
+  );
 }
