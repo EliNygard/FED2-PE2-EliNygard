fa0f7535005a59df8414c30bfc64d3d5a7cfae10
EliNygard <eli.nygard@gmail.com>
2025-04-27 16:09:35 +0200

    get booked dates and disable in calendar

--- src/ui/venuePage/VenueCalendar/index.tsx
+++ src/ui/venuePage/VenueCalendar/index.tsx
@@ -8,20 +8,32 @@ import { DateRange } from "react-day-picker";
 export default function VenueCalendar({ venue }: { venue: IVenue }) {
   const [date, setDate] = useState<DateRange | undefined>();
   const bookings = venue.bookings;
+  const bookedPeriods = bookings.map((booking) => ({
+    from: new Date(booking.dateFrom),
+    to: new Date (booking.dateTo)
+  }))
+
+  
   console.log(bookings);
   console.log(date);
+  console.log(date?.from?.toISOString());
+  console.log(date?.to?.toISOString());
 
   const handleSelect = (newSelected: DateRange | undefined) => {
     setDate(newSelected);
   };
 
   return (
-    <Calendar
-      mode="range"
-      selected={date}
-      disabled={{ before: new Date() }}
-      onSelect={handleSelect}
-      className="rounded-md border"
-    />
+    <>
+      <Calendar
+        mode="range"
+        selected={date}
+        disabled={[{ before: new Date() }, ...bookedPeriods]}
+        excludeDisabled
+        onSelect={handleSelect}
+        className="rounded-md border"
+      />
+      <div>{date ? `Selected date: ${date}` : "Select dates"}</div>
+    </>
   );
 }
