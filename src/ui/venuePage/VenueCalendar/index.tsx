74152a4cbbc55d2672c8042cf78e7d6defa952d6
EliNygard <eli.nygard@gmail.com>
2025-04-27 17:44:43 +0200

    working on calendar functionality

--- src/ui/venuePage/VenueCalendar/index.tsx
+++ src/ui/venuePage/VenueCalendar/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { IVenue } from "@/interface";
+import { differenceInCalendarDays } from "date-fns";
 import { useState } from "react";
 import { DateRange, DayPicker } from "react-day-picker";
 
@@ -9,35 +10,49 @@ export default function VenueCalendar({ venue }: { venue: IVenue }) {
   const bookings = venue.bookings;
   const bookedPeriods = bookings.map((booking) => ({
     from: new Date(booking.dateFrom),
-    to: new Date (booking.dateTo)
-  }))
-
-
-  
-  console.log(bookings);
-  console.log(date);
-  console.log(date?.from?.toISOString());
-  console.log(date?.to?.toISOString());
+    to: new Date(booking.dateTo),
+  }));
 
   const handleSelect = (newSelected: DateRange | undefined) => {
     setDate(newSelected);
   };
 
+  const amountNights =
+    date?.from && date?.to
+      ? differenceInCalendarDays(date.to, date.from) + 1
+      : undefined;
+
+  console.log(amountNights);
+
+  const pricePerNight = venue.price
+  if (amountNights) {
+    const totalCost = pricePerNight * amountNights
+    console.log(totalCost);
+  }
+  
+
+  console.log(date);
+  // console.log(date?.from?.getTime());
+  // console.log(date?.to?.getTime());
+
   return (
     <>
-    <div className="rounded-lg border-brand-blue w-full overflow-x-auto p-0.5 calendar-container">
-
-      <DayPicker
-        mode="range"
-        selected={date}
-        disabled={[{ before: new Date() }, ...bookedPeriods]}
-        excludeDisabled
-        numberOfMonths={1}
-        onSelect={handleSelect}
-        // className="rounded-md border"
+      <h2 className="mb-4">Select dates to see the price</h2>
+      <div className="rounded-lg border-brand-blue border p-2 w-full overflow-x-auto calendar-container">
+        <DayPicker
+          mode="range"
+          selected={date}
+          disabled={[{ before: new Date() }, ...bookedPeriods]}
+          excludeDisabled
+          numberOfMonths={1}
+          onSelect={handleSelect}
+          modifiers={{ booked: bookedPeriods }}
+          modifiersClassNames={{
+            booked: "bg-red-200 cursor-not-allowed",
+          }}
         />
-        </div>
-      <div>{date ? `Selected date: ${date}` : "Select dates"}</div>
+      </div>
+      <div>{amountNights ? `Total` : "Select dates"}</div>
     </>
   );
 }
