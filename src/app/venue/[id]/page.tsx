381bb0412bd846e02a143a5aee3831b4d9119adc
EliNygard <eli.nygard@gmail.com>
2025-04-27 12:57:28 +0200

    adds calendar component

--- src/app/venue/[id]/page.tsx
+++ src/app/venue/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { getVenueById, getVenues } from "@/lib/api";
+import VenueCalendar from "@/ui/venuePage/VenueCalendar";
 import VenueHeader from "@/ui/venuePage/VenueHeader";
 import VenueInfo from "@/ui/venuePage/VenueInfo";
 import VenueLocation from "@/ui/venuePage/VenueLocation";
@@ -32,7 +33,7 @@ export default async function VenuePage({
         <VenueInfo venue={venue} />
       </div>
       <div className="md:col-start-2 md:row-start-3 md:row-end-5">
-        04 Calendar
+        <VenueCalendar venue={venue} />
       </div>
       <div className="md:col-start-1">
         <VenueLocation venue={venue} />
