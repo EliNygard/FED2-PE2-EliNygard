83162c6bbbb0e3b1e111dc43efcfb2618f465e8b
EliNygard <eli.nygard@gmail.com>
2025-04-28 09:41:29 +0200

    calc total cost, adds styling

--- src/ui/venuePage/VenueCalendar/index.tsx
+++ src/ui/venuePage/VenueCalendar/index.tsx
@@ -5,6 +5,14 @@ import { differenceInCalendarDays } from "date-fns";
 import { useState } from "react";
 import { DateRange, DayPicker } from "react-day-picker";
 
+// TODO:
+// calculate total ✅
+// verification: must select min one night + error message
+// store selected dates, nights, guests, price, total
+// add buttons, disable if no token
+// style calendar border
+// create booking confirmation
+
 export default function VenueCalendar({ venue }: { venue: IVenue }) {
   const [selectedDate, setDate] = useState<DateRange | undefined>();
 
@@ -24,10 +32,11 @@ export default function VenueCalendar({ venue }: { venue: IVenue }) {
       : undefined;
 
   const pricePerNight = venue.price;
-  // const total = pricePerNight * amountBookedNights;
-  // console.log(total);
 
-  // verification: must select min one night
+  const totalCost = amountBookedNights
+    ? pricePerNight * amountBookedNights
+    : null;
+
   if (selectedDate?.to === selectedDate?.from) {
     console.log("please select min one night");
   }
@@ -38,27 +47,32 @@ export default function VenueCalendar({ venue }: { venue: IVenue }) {
   }
 
   console.log(selectedDate);
-  // console.log(date?.from?.getTime());
-  // console.log(date?.to?.getTime());
+  console.log(selectedDate?.from?.toLocaleDateString());
+  console.log(selectedDate?.to?.toLocaleDateString());
 
   return (
-    <>
-      <h2 className="mb-4">Select dates to see the price</h2>
-      <div className="rounded-lg border-brand-blue border p-2 w-full overflow-x-auto calendar-container">
-        <DayPicker
-          mode="range"
-          selected={selectedDate}
-          disabled={[{ before: new Date() }, ...bookedPeriods]}
-          excludeDisabled
-          numberOfMonths={1}
-          onSelect={handleSelect}
-          modifiers={{ booked: bookedPeriods }}
-          modifiersClassNames={{
-            booked: "bg-red-200 cursor-not-allowed",
-          }}
-        />
+    <section>
+      <h2>Select dates to see the price</h2>
+      <div className="mt-4 flex flex-col gap-5">
+        <div className="rounded-lg border-brand-blue border p-2 w-full overflow-x-auto calendar-container">
+          <DayPicker
+            mode="range"
+            selected={selectedDate}
+            disabled={[{ before: new Date() }, ...bookedPeriods]}
+            excludeDisabled
+            numberOfMonths={1}
+            onSelect={handleSelect}
+            modifiers={{ booked: bookedPeriods }}
+            modifiersClassNames={{
+              booked: "bg-red-200 cursor-not-allowed",
+            }}
+          />
+        </div>
+        
+        <div className="uppercase" style={{ fontWeight: 500 }}>
+          {amountBookedNights ? `Total ${totalCost} NOK` : " "}
+        </div>
       </div>
-      <div>{amountBookedNights ? `Total NOK` : " "}</div>
-    </>
+    </section>
   );
 }
