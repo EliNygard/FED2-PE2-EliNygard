e7de99c309c016a0a562b672064ee870da27d08c
EliNygard <eli.nygard@gmail.com>
2025-04-28 09:36:13 +0200

    improves semantics, adds some styling

--- src/ui/venuePage/VenueInfo/index.tsx
+++ src/ui/venuePage/VenueInfo/index.tsx
@@ -4,18 +4,26 @@ export default function VenueInfo({ venue }: { venue: IVenue }) {
   return (
     <section>
       <h2>About the venue</h2>
-      <div className="mt-4 flex flex-col gap-1.5">
-        <p>
-          {`${venue.maxGuests} guests`}
-        </p>
-        <p>
-          {venue.meta.parking ? 'Parking included' : ''} 
-          </p>
-        <p>{venue.meta.wifi ? 'Free wifi' : ''}</p>
-        <p>{venue.meta.breakfast ? 'Breakfast included' : ''}</p>
-        <p>{venue.meta.pets ? 'Pets are welcome' : ''}</p>
+      <div className="mt-4 flex flex-col gap-5">
+        <ul className="flex flex-col gap-1.5">
+          <li>
+            <p>{`${venue.maxGuests} guests`}</p>
+          </li>
+          <li>
+            <p>{venue.meta.parking ? "Parking included" : ""}</p>
+          </li>
+          <li>
+            <p>{venue.meta.wifi ? "Free wifi" : ""}</p>
+          </li>
+          <li>
+            <p>{venue.meta.breakfast ? "Breakfast included" : ""}</p>
+          </li>
+          <li>
+            <p>{venue.meta.pets ? "Pets are welcome" : ""}</p>
+          </li>
+        </ul>
+        <p className="mt-3">{venue.description}</p>
       </div>
-      <p className="mt-6">{venue.description}</p>
     </section>
   );
 }
