61652226a980660fc2181b316223392b75be3050
EliNygard <eli.nygard@gmail.com>
2025-05-01 11:20:53 +0200

    set up features for venue mangers

--- src/ui/ProfileHeader/index.tsx
+++ src/ui/ProfileHeader/index.tsx
@@ -1,6 +1,15 @@
-export default function ProfileHeader() {
-  return(
-    <div className="bg-secondary-background">Profile header</div>
+"use client";
 
-  )
+import { useAuthStore } from "@/stores/useAuthStore";
+import { useParams } from "next/navigation";
+
+export default function ProfileHeader() {
+  const { username } = useParams();
+  const isVenueManager = useAuthStore((state) => state.isVenueManager);
+  return (
+    <section className="bg-secondary-background">
+      <h1>Hello, {username}</h1>
+      {isVenueManager ? <h2>Venue Manager</h2> : ""}
+    </section>
+  );
 }
--- src/ui/ProfileNav/index.tsx
+++ src/ui/ProfileNav/index.tsx
@@ -1,11 +1,21 @@
+'use client'
+
+import { useAuthStore } from "@/stores/useAuthStore";
+
 export default function ProfileNav() {
+const isVenueManager = useAuthStore((state) => state.isVenueManager);
   return (
-    <div className="bg-amber-600">
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
+    <div className="flex gap-3.5 flex-wrap">
+      {isVenueManager ? (
+        <>
+        <div>Create New Venue</div>
+        <div>My Venues</div>
+        </>
+
+      ) : ''}
+      <div>My Bookings</div>
+      <div>Edit Profile</div>
+      <div>Log out</div>
     </div>
   );
 }
