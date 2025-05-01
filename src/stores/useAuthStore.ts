70669075f6c4f499d1180e9c4048738b9854a883
EliNygard <eli.nygard@gmail.com>
2025-05-01 12:55:04 +0200

    refactor store and adds persist

--- src/stores/useAuthStore.ts
+++ src/stores/useAuthStore.ts
@@ -1,35 +1,98 @@
 import { IUser } from "@/interface";
 import { create } from "zustand";
-import { subscribeWithSelector } from "zustand/middleware";
+import { persist, subscribeWithSelector } from "zustand/middleware";
+import { login as apiLogin } from "@/app/api/auth/login";
 
 interface AuthState {
   user: IUser | null;
+  token: string | null;
+  isAuthenticated: boolean;
   isVenueManager: boolean;
   setUser: (user: IUser) => void;
+  login: (email: string, password: string) => Promise<void>;
   logout: () => void;
 }
 
-export const useAuthStore = create(
-  subscribeWithSelector<AuthState>((set) => ({
-    user: null,
-    isVenueManager: false,
-    setUser: (user) =>
-      set({
-        user: user,
-        isVenueManager: !!user.venueManager,
-      }),
+const initialState = {
+  user: null,
+  token: null,
+  isAuthenticated: false,
+  isVenueManager: false,
+};
 
-    logout: () => {
-      set({ user: null, isVenueManager: false });
-    },
-  }))
+export const useAuthStore = create<AuthState>()(
+  persist(
+    subscribeWithSelector((set, get) => ({
+      ...initialState,
+
+      
+      setUser: (user) =>
+        set({
+          user,
+          token: user.accessToken,
+          isAuthenticated: true,
+          isVenueManager: !!user.venueManager,
+        }),
+
+        login: async (email, password) => {
+          try {
+            const { user, accessToken } = await apiLogin({email, password})
+            set({
+              user, token: accessToken, isAuthenticated: true, isVenueManager: !!user.venueManager,
+            })
+          } catch (error) {
+            console.error('Login failed', error);
+            
+          }
+        },
+
+        changeToManager: () => {
+          // do something here if user edits profile and change to venue manager?
+        },
+
+        logout:  () => {
+          if (!get().token) return
+          set({ user: null, token: null, isVenueManager: false, isAuthenticated: false, });
+        }
+    })),
+    {
+      name: "auth-storage",
+      partialize: (state) => ({
+        user: state.user,
+        token: state.token,
+        isAuthenticated: state.isAuthenticated,
+        isVenueManager: state.isVenueManager,
+      }),
+    }
+  )
 );
+
+// export const useAuthStore = create<AuthState>()(
+//   persist(
+//   subscribeWithSelector((set) => ({
+//     user: null,
+//     isVenueManager: false,
+//     setUser: (user) =>
+//       set({
+//         user,
+//         isVenueManager: !!user.venueManager,
+//       }),
+
+//     logout: () => {
+//       set({ user: null, isVenueManager: false });
+//     },
+//   })),
+//   {
+//     name: 'auth-storage',
+
+//   }),
+// );
 
 // Subscribe to state changes and log them
 useAuthStore.subscribe((state) => {
   console.log("[auth store] state changed: ", state);
 });
 
-export function getToken() {
-  return useAuthStore.getState().user?.accessToken
-}
+// export function getToken() {
+//   return useAuthStore.getState().user?.accessToken
+// }
