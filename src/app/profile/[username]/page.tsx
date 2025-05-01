f2d2014893ea58f25ebdcabcef9bdd209f9b407b
EliNygard <eli.nygard@gmail.com>
2025-05-01 09:16:28 +0200

    new path for profile page

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -1,7 +1,7 @@
 export default function ProfilePage() {
     return (
         <section>
-            Here comes the different sections
+            Here comes the different profile page sections
         </section>
     )
 }
