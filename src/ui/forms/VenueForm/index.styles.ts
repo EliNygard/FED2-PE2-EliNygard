0623d48369ef781c60348c56d5d9f980f5be6ce5
EliNygard <eli.nygard@gmail.com>
2025-05-10 11:28:36 +0200

    styles venue form

---
+++ src/ui/forms/VenueForm/index.styles.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const StyledVenueForm = styled.div`
+margin-top: 2rem;
+display: flex;
+flex-direction: column;
+gap: 1.5rem;
+
+p {
+  font-size: 14px;
+}
+`;
+
+export const StyledFieldset = styled.fieldset`
+display: grid;
+gap: .5rem;
+`
+
+export const StyledLegend = styled.legend`
+
+`
+
--- src/ui/forms/VenueForm/index.tsx
+++ src/ui/forms/VenueForm/index.tsx
@@ -6,10 +6,8 @@ import { Checkbox } from "@/components/ui/checkbox";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
-  FormLabel,
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
@@ -18,6 +16,7 @@ import { venueFormSchema, VenueFormValues } from "@/lib/schemas";
 import Button from "@/ui/Button";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useFieldArray, useForm } from "react-hook-form";
+import { StyledFieldset, StyledVenueForm } from "./index.styles";
 
 const meta = [
   {
@@ -86,265 +85,284 @@ export default function VenueForm() {
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)}>
-        {/* Venue Name */}
+        <StyledVenueForm>
+          {/* Venue Name */}
 
-        <FormField
-          control={form.control}
-          name="name"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>
-                The name of the venue
-                <span aria-hidden="true">*</span>
-                <span className="sr-only">(required)</span>
-              </FormLabel>
-              <FormDescription>
-                Pick a memorable name that captures your venue&apos;s unique
-                charm.
-              </FormDescription>
-              <FormControl>
-                <Input {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <label>
+                  The name of the venue
+                  <span aria-hidden="true"> *</span>
+                  <span className="sr-only">(required)</span>
+                </label>
+                <p>
+                  Pick a memorable name that captures your venue&apos;s unique
+                  charm.
+                </p>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-        {/* Description */}
+          {/* Description */}
 
-        <FormField
-          control={form.control}
-          name="description"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>
-                Description
-                <span aria-hidden="true">*</span>
-                <span className="sr-only">(required)</span>
-              </FormLabel>
-              <FormDescription>
-                Share a brief overview highlighting your venue&apos;s key
-                features and inviting atmosphere.
-              </FormDescription>
-              <FormControl>
-                <Textarea {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <label>
+                  Description
+                  <span aria-hidden="true">*</span>
+                  <span className="sr-only">(required)</span>
+                </label>
+                <p>
+                  Share a brief overview highlighting your venue&apos;s key
+                  features and inviting atmosphere.
+                </p>
+                <FormControl>
+                  <Textarea {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-        {/* Images */}
+          {/* Images */}
 
-        <FormLabel>Add images of your venue</FormLabel>
-        <FormDescription>
-          Show off your space! Upload a few photos that capture the unique style
-          and inviting atmosphere of your venue.
-        </FormDescription>
+          <StyledFieldset>
+            {/* <StyledLegend> */}
+            {/* <FormItem> */}
+            <h3>Add images of your venue</h3>
+            <p>
+              Show off your space! Upload a few photos that capture the unique
+              style and inviting atmosphere of your venue. We reccomend minimum five images.
+            </p>
+            {/* </FormItem> */}
+            {/* </StyledLegend> */}
 
-        {fields.map((field, index) => {
-          return (
-            <div key={field.id}>
-              <FormField
-                control={form.control}
-                name={`media.${index}.url`}
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>{`Image ${index + 1}`}</FormLabel>
-                    <FormControl>
-                      <Input {...field} type="url" />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name={`media.${index}.alt`}
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>{`Describe image ${index + 1}`}</FormLabel>
-                    <FormControl>
-                      <Input {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              {fields.length > 1 && (
-                <button type="button" onClick={() => remove(index)}>
-                  Remove
-                </button>
-              )}
-            </div>
-          );
-        })}
-
-        <button
-          type="button"
-          onClick={() => {
-            append({
-              url: "",
-              alt: "",
-            });
-          }}
-        >
-          Add image
-        </button>
-
-        {/* Price per night */}
-
-        <FormField
-          control={form.control}
-          name="rate"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>
-                Price per night
-                <span aria-hidden="true">*</span>
-                <span className="sr-only">(required)</span>
-              </FormLabel>
-              <FormDescription>
-                Enter the base rate each guest will pay per night. This helps
-                set clear financial expectations for your bookings.
-              </FormDescription>
-              <FormControl>
-                <Input type="number" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-        {/* Guests */}
-
-        <FormField
-          control={form.control}
-          name="guests"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>
-                Maximum number of guests
-                <span aria-hidden="true">*</span>
-                <span className="sr-only">(required)</span>
-              </FormLabel>
-              <FormDescription>
-                Specify the highest number of people your venue can comfortably
-                accommodate. This allows you to manage capacity and ensures a
-                safe, enjoyable experience.
-              </FormDescription>
-              <FormControl>
-                <Input type="number" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-        {/* Facilities */}
-
-        <FormField
-          control={form.control}
-          name="meta"
-          render={() => (
-            <FormItem>
-              <FormLabel>Facilities at your venue</FormLabel>
-              <FormDescription>
-                Select the amenities that make your venue extra inviting. Check
-                all that apply.
-              </FormDescription>
-              {meta.map((m) => (
-                <FormField
-                  key={m.id}
-                  control={form.control}
-                  name="meta"
-                  render={({ field }) => {
-                    return (
-                      <FormItem key={m.id}>
+            {fields.map((field, index) => {
+              return (
+                <div key={field.id} className="grid gap-2 mb-2">
+                  <FormField
+                    control={form.control}
+                    name={`media.${index}.url`}
+                    render={({ field }) => (
+                      <FormItem>
+                        <label>{`Image ${index + 1}`}</label>
                         <FormControl>
-                          <Checkbox
-                            checked={field.value?.includes(m.id)}
-                            onCheckedChange={(checked) => {
-                              return checked
-                                ? field.onChange([...field.value, m.id])
-                                : field.onChange(
-                                    field.value?.filter(
-                                      (value) => value === m.id
-                                    )
-                                  );
-                            }}
-                          />
+                          <Input {...field} type="url" />
                         </FormControl>
-                        <FormLabel>{m.label}</FormLabel>
+                        <FormMessage />
                       </FormItem>
-                    );
-                  }}
-                />
-              ))}
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name={`media.${index}.alt`}
+                    render={({ field }) => (
+                      <FormItem>
+                        <label>{`Describe image ${index + 1}`}</label>
+                        <FormControl>
+                          <Input {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  {fields.length > 1 && (
+                    <div className="w-1/3">
+                      <Button
+                        $variant="narrow"
+                        className="bg-primary-font"
+                        type="button"
+                        onClick={() => remove(index)}
+                      >
+                        Remove
+                      </Button>
+                    </div>
+                  )}
+                </div>
+              );
+            })}
 
-        {/* Location */}
+            <Button
+              type="button"
+              $variant="narrow"
+              className="bg-brand-blue"
+              onClick={() => {
+                append({
+                  url: "",
+                  alt: "",
+                });
+              }}
+            >
+              Add new image
+            </Button>
+          </StyledFieldset>
 
-        <FormLabel>Location details</FormLabel>
-        <FormDescription>Enter your venue&apos;s address</FormDescription>
+          {/* Price per night */}
 
-        <FormField
-          control={form.control}
-          name="location.address"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Street</FormLabel>
-              <FormControl>
-                <Input {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="location.city"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>City</FormLabel>
-              <FormControl>
-                <Input {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="location.zip"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Zip code</FormLabel>
-              <FormControl>
-                <Input {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="location.country"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Country</FormLabel>
-              <FormControl>
-                <Input {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+          <FormField
+            control={form.control}
+            name="rate"
+            render={({ field }) => (
+              <FormItem>
+                <label>
+                  Price per night
+                  <span aria-hidden="true">*</span>
+                  <span className="sr-only">(required)</span>
+                </label>
+                <p>
+                  Enter the base rate each guest will pay per night. This helps
+                  set clear financial expectations for your bookings.
+                </p>
+                <FormControl>
+                  <Input type="number" {...field} className="w-1/3" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-        <Button type="submit">Submit</Button>
+          {/* Guests */}
+
+          <FormField
+            control={form.control}
+            name="guests"
+            render={({ field }) => (
+              <FormItem>
+                <label>
+                  Maximum number of guests
+                  <span aria-hidden="true">*</span>
+                  <span className="sr-only">(required)</span>
+                </label>
+                <p>
+                  Specify the highest number of people your venue can
+                  comfortably accommodate. This allows you to manage capacity
+                  and ensures a safe, enjoyable experience.
+                </p>
+                <FormControl>
+                  <Input type="number" {...field} className="w-1/3" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          {/* Facilities */}
+
+          <FormField
+            control={form.control}
+            name="meta"
+            render={() => (
+              <FormItem>
+                <label>Facilities at your venue</label>
+                <p>
+                  Select the amenities that make your venue extra inviting.
+                  Check all that apply.
+                </p>
+                {meta.map((m) => (
+                  <FormField
+                    key={m.id}
+                    control={form.control}
+                    name="meta"
+                    render={({ field }) => {
+                      return (
+                        <FormItem key={m.id} className="flex flex-row gap-3 items-center justify-start">
+                          <FormControl>
+                            <Checkbox
+                              checked={field.value?.includes(m.id)}
+                              onCheckedChange={(checked) => {
+                                return checked
+                                  ? field.onChange([...field.value, m.id])
+                                  : field.onChange(
+                                      field.value?.filter(
+                                        (value) => value !== m.id
+                                      )
+                                    );
+                              }}
+                            />
+                          </FormControl>
+                          <label>{m.label}</label>
+                        </FormItem>
+                      );
+                    }}
+                  />
+                ))}
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          {/* Location */}
+
+          <StyledFieldset>
+            <h3>Location details</h3>
+            <p>Enter your venue&apos;s address</p>
+
+            <FormField
+              control={form.control}
+              name="location.address"
+              render={({ field }) => (
+                <FormItem>
+                  <label>Street</label>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="location.city"
+              render={({ field }) => (
+                <FormItem>
+                  <label>City</label>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="location.zip"
+              render={({ field }) => (
+                <FormItem>
+                  <label>Zip code</label>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="location.country"
+              render={({ field }) => (
+                <FormItem>
+                  <label>Country</label>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </StyledFieldset>
+
+          <Button type="submit">Submit</Button>
+        </StyledVenueForm>
       </form>
     </Form>
   );
