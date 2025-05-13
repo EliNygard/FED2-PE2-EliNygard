3da281d630a8aa6c351dbb9d52b5bba590135fff
EliNygard <eli.nygard@gmail.com>
2025-05-13 11:50:29 +0200

    adds login button component

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -8,6 +8,7 @@ import {
 } from "@/components/ui/dialog";
 import Button from "@/ui/Button";
 import LoginForm from "@/ui/forms/LoginForm";
+import LoginButton from "@/ui/LoginButton";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -21,32 +22,7 @@ export default function LoginPage() {
 
     <ul className="flex gap-5 items-center m-auto ">
         <li>
-          <Dialog>
-            <DialogTrigger asChild>
-              <Button>Log in</Button>
-            </DialogTrigger>
-            <DialogContent className="bg-secondary-background max-h-[calc(100vh-4rem)] overflow-y-auto touch-pan-y">
-              <div className="grid grid-cols-2">
-                <div className="py-4 pr-8 mt-11">
-                  <DialogTitle className="text-xl mb-10">
-                    Log in to your account
-                  </DialogTitle>
-                  <DialogDescription className="hidden"></DialogDescription>
-                  <LoginForm />
-                </div>
-                <DialogHeader>
-                  <Image
-                    src="/LogoMountainsH.svg"
-                    alt="Welcome to Holidaze"
-                    width={321}
-                    height={214}
-                    className="w-full h-auto object-contain"
-                    priority
-                  />
-                </DialogHeader>
-              </div>
-            </DialogContent>
-          </Dialog>
+          <LoginButton />
         </li>
         <li>
           <p>OR</p>
