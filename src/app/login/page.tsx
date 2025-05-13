9aab43105b8ffccb1c29c146ad1e3218102f22c4
EliNygard <eli.nygard@gmail.com>
2025-05-13 11:51:07 +0200

    removes unused imports

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,26 +1,13 @@
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import Button from "@/ui/Button";
-import LoginForm from "@/ui/forms/LoginForm";
 import LoginButton from "@/ui/LoginButton";
-import Image from "next/image";
 import Link from "next/link";
 
 export default function LoginPage() {
   return (
     <section className="max-w-4xl m-auto page-padding flex flex-col gap-5">
-      <h1>
-        Log in to your Holidaze profile
-      </h1>
+      <h1>Log in to your Holidaze profile</h1>
       <p>Find and book your next stay at one of our venues.</p>
 
-    <ul className="flex gap-5 items-center m-auto ">
+      <ul className="flex gap-5 items-center m-auto ">
         <li>
           <LoginButton />
         </li>
@@ -28,9 +15,9 @@ export default function LoginPage() {
           <p>OR</p>
         </li>
         <li>
-            <Link href="/register">Register</Link>
+          <Link href="/register">Register</Link>
         </li>
       </ul>
     </section>
-  )
+  );
 }
