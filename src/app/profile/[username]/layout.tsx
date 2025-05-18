86c38bb3abe9aa8376b5928f7df5a75704c2cbad
EliNygard <eli.nygard@gmail.com>
2025-05-18 20:20:45 +0200

    adds metadata

--- src/app/profile/[username]/layout.tsx
+++ src/app/profile/[username]/layout.tsx
@@ -3,6 +3,28 @@ import ProfileHeader from "@/ui/ProfileHeader";
 import ProfileNav from "@/ui/ProfileNav";
 import { Suspense } from "react";
 
+import type { Metadata } from "next";
+
+type Props = {
+  params: Promise<{ username: string }>;
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+};
+
+export async function generateMetadata(
+  { params }: Props,
+  
+): Promise<Metadata> {
+  const username = (await params).username;
+
+  // fetch post information
+  // const venue = (await getVenueById(id)).data;
+
+  return {
+    title: `Profile page ${username} `,
+    description: `${username}'s profile page`,
+  };
+}
+
 /**
  * Layout component for /profile pages.
  *
