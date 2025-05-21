9dc92e2f9f182b9ce7ab06e8a7864e1bcb80f572
EliNygard <eli.nygard@gmail.com>
2025-05-21 22:48:02 +0200

    renders null if no meta on venue info

--- src/ui/venuePage/VenueInfo/index.tsx
+++ src/ui/venuePage/VenueInfo/index.tsx
@@ -1,12 +1,14 @@
 import { IVenue } from "@/interface";
 
 /**
- * VenueInfo displays info about the venue to the user. 
+ * VenueInfo displays info about the venue to the user.
  * - The component is part of the VenuePage.
  * @param venue The venue data.
  */
 
 export default function VenueInfo({ venue }: { venue: IVenue }) {
+  console.log(venue.meta.parking);
+
   return (
     <section>
       <h2>About the venue</h2>
@@ -15,18 +17,26 @@ export default function VenueInfo({ venue }: { venue: IVenue }) {
           <li>
             <p>{`${venue.maxGuests} guests`}</p>
           </li>
-          <li>
-            <p>{venue.meta.parking ? "Parking included" : ""}</p>
-          </li>
-          <li>
-            <p>{venue.meta.wifi ? "Free wifi" : ""}</p>
-          </li>
-          <li>
-            <p>{venue.meta.breakfast ? "Breakfast included" : ""}</p>
-          </li>
-          <li>
-            <p>{venue.meta.pets ? "Pets are welcome" : ""}</p>
-          </li>
+          {venue.meta.parking && (
+            <li>
+              <p>Parking included</p>
+            </li>
+          )}
+          {venue.meta.wifi && (
+            <li>
+              <p>Free wifi</p>
+            </li>
+          )}
+          {venue.meta.breakfast && (
+            <li>
+              <p>Breakfast included</p>
+            </li>
+          )}
+          {venue.meta.pets && (
+            <li>
+              <p>Pets are welcome</p>
+            </li>
+          )}
         </ul>
         <p className="mt-3 capitalize">{venue.description}</p>
       </div>
