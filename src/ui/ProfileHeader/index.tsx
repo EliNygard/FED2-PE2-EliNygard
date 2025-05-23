a1999a694e013627595027f01cc8137055bd3321
EliNygard <eli.nygard@gmail.com>
2025-05-23 16:55:52 +0200

    changes element types

--- src/ui/ProfileHeader/index.tsx
+++ src/ui/ProfileHeader/index.tsx
@@ -7,13 +7,13 @@ export default function ProfileHeader() {
   const { username } = useParams();
   const isVenueManager = useAuthStore((state) => state.isVenueManager);
   return (
-    <section className="bg-secondary-background flex flex-wrap gap-2.5 justify-between items-center py-6 px-6 md:px-10 lg:px-10 2xl:px-20">
-      <h1 className="text-sm md:text-lg">Hello, {username}</h1>
+    <header className="bg-secondary-background flex flex-wrap gap-2.5 justify-between items-center py-6 px-6 md:px-10 lg:px-10 2xl:px-20">
+      <div className="text-sm md:text-lg">Hello, {username}</div>
       {isVenueManager ? (
-        <h2 className="text-xs md:text-sm uppercase">Venue Manager</h2>
+        <div className="text-xs md:text-sm uppercase">Venue Manager</div>
       ) : (
         ""
       )}
-    </section>
+    </header>
   );
 }
